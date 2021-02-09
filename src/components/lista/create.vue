70cd0e703fc90e47b98e2ad688f3db9d82ee3979
Escandon <aleskndon@hotmail.com>
2021-02-09 01:48:26 -0600

    feat: new & edit

--- src/components/lista/create.vue
+++ src/components/lista/create.vue
@@ -18,14 +18,29 @@
 						placeholder="Descripción"
 						required
 					></b-form-input>
+					<!-- <template v-if="type == 1"> -->
+						<b-form-checkbox
+							id="checkbox-1"
+							v-model="producto.activo"
+							name="checkbox-1"
+							value="1"
+						>
+							Activo
+						</b-form-checkbox>
+					<!-- </template> -->
 				</b-container>
 			</div>
+			<template #modal-footer>
+				<b-button size="sm" variant="primary" @click="accion">
+					{{ type == 1 ? 'Actualizar'  : 'Crear'}}
+				</b-button>
+			</template>
 		</b-modal>
 	</div>
 </template>
 
 <script>
-import { newProduct } from "@/services/product";
+import { newProduct, editProducto } from "@/services/product";
 export default {
 	props: {
 		type: 0,
@@ -34,7 +49,7 @@ export default {
 	},
 	data() {
 		return {
-			producto: Object,
+			producto: '',
 		};
 	},
 	methods: {
@@ -49,6 +64,29 @@ export default {
 				})
 				.catch((erro) => console.log(erro));
 		},
+		editar(){
+			editProducto(this.producto).then((res) => {
+				this.$bvToast.toast(res.data.message, {
+						title: "Producto",
+						autoHideDelay: 5000,
+						appendToast: false,
+					});
+			}).catch((err) => {
+				this.$bvToast.toast("algo ha salido mal", {
+						title: "Producto",
+						autoHideDelay: 5000,
+						appendToast: false,
+					});
+			})
+		},
+
+		accion(){
+			if(this.type ==  1){
+				this.editar()
+			}else{
+				this.nuevo()
+			}
+		}
 	},
 	computed: {
 		visualizar: {
@@ -59,17 +97,17 @@ export default {
 				this.$emit("modalClose", valor);
 			},
 		},
-		titulo(){
-			if(this.type == 0){
-				return 'Nuevo Producto'
+		titulo() {
+			if (this.type == 0) {
+				return "Nuevo Producto";
 			}
-			return 'Editar Producto'
-		}
+			return "Editar Producto";
+		},
 	},
 	watch: {
 		productC(newValue, oldValue) {
-			this.producto = newValue
-		}
+			this.producto = newValue;
+		},
 	},
 };
 </script>
