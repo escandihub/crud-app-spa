15f208a79114acfb7432511b08182ff401eacda7
Escandon <aleskndon@hotmail.com>
2021-02-09 10:13:23 -0600

    update: register path login

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */'@/components/login'),
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
