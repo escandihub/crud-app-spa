b68ab916cd1a483adafdc48245a1daf633793906
Escandon <aleskndon@hotmail.com>
2021-02-12 11:50:09 -0600

    fix: token

--- src/services/API.js
+++ src/services/API.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 export default () => {
 	const instancia = axios.create({
 		// baseURL: window.globalConfig || process.env.VUE_APP_URL_API
-		baseURL: "http://127.0.0.1:8000"
+		baseURL: "http://localhost:8000",
+		headers: {
+			'Cross-Origin-Opener-Policy': '*',
+			'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Headers': '*'},
 	})
 	instancia.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('access_token')}`
-	
+	instancia.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+	instancia.defaults.headers.common['Access-Control-Allow-Headers'] = '*';
 	instancia.interceptors.response.use((resp) => { 
 		return resp
 	})
